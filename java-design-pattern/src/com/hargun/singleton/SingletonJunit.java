39fd623ac0d356e2bc5631e33c415a56c5eb4cea
HargunSinghSuri <hargun.15@gmail.com>
2019-05-09 12:46:56 +0530

    Junit Test included

--- java-design-pattern/src/com/hargun/singleton/SingletonJunit.java
+++ java-design-pattern/src/com/hargun/singleton/SingletonJunit.java
@@ -1,5 +1,20 @@
 package com.hargun.singleton;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 public class SingletonJunit {
+
+	@Test
+	public void compareObject() {
+
+		// Get 2 object for comparision
+		SingletonExample objOne = SingletonExample.getSingletonObject();
+		SingletonExample objTwo = SingletonExample.getSingletonObject();
+
+		// Perform Test
+		Assert.assertEquals("Objects are equal", objOne, objTwo);
+
+	}
 
 }
